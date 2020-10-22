2e100ea4310ac1cf10163e90a3a4c914503657be
Your Name <you@example.com>
2020-10-22 09:06:52 +0545

    axios example addesd

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hello world')
+const axios = require('axios'),
+// console.log(axios); 
+
+// Make a request for a user with a given ID
+axios.get('/user?ID=12345')
+  .then(function (response) {
+    // handle success
+    response.data.forEach(function(value,index){
+    	console.log(value);
+
+    }) 
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
